1e1969f7b0853b608d63ea89fc35f35e85c86707
myfreeer <myfreeer@users.noreply.github.com>
2017-09-11 08:45:21 +0800

    fix monitoroff

--- monitoroff.c
+++ monitoroff.c
@@ -1,6 +1,6 @@
 #include <windows.h>
-#define MONITOR_ON 2
-#define MONITOR_OFF -1
+#define MONITOR_OFF 2
+#define MONITOR_ON -1
 #define MONITOR_STANBY 1
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
